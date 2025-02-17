AntumDeluge/mtmp-mydoors

[misc] Use 'coloredwood' in place of 'my_door_wood'

my_misc_doors/locked.lua
@@ -30,18 +30,18 @@ end
 minetest.register_craft({
 	output = "my_misc_doors:door1_locked 1",
 	recipe = {
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", "default:steel_ingot"},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""}
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", "default:steel_ingot"},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""}
 	}
 })
 
 minetest.register_craft({
 	output = "my_misc_doors:door2_locked 1",
 	recipe = {
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", "default:steel_ingot"},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""}
+		{"coloredwood:wood_gray", "coloredwood:wood_gray", ""},
+		{"coloredwood:wood_gray", "coloredwood:wood_gray", "default:steel_ingot"},
+		{"coloredwood:wood_gray", "coloredwood:wood_gray", ""}
 	}
 })
 minetest.register_craft({
@@ -63,9 +63,9 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "my_misc_doors:door5_locked 1",
 	recipe = {
-		{"my_door_wood:wood_white", "wool:red", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", "default:steel_ingot"},
-		{"my_door_wood:wood_white", "wool:red", ""}
+		{"coloredwood:wood_white", "wool:red", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", "default:steel_ingot"},
+		{"coloredwood:wood_white", "wool:red", ""}
 	}
 })
 minetest.register_craft({

my_misc_doors/unlocked.lua
@@ -29,18 +29,18 @@ end
 minetest.register_craft({
 	output = "my_misc_doors:door1 1",
 	recipe = {
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""}
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""}
 	}
 })
 
 minetest.register_craft({
 	output = "my_misc_doors:door2 1",
 	recipe = {
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""}
+		{"coloredwood:wood_gray", "coloredwood:wood_gray", ""},
+		{"coloredwood:wood_gray", "coloredwood:wood_gray", ""},
+		{"coloredwood:wood_gray", "coloredwood:wood_gray", ""}
 	}
 })
 minetest.register_craft({
@@ -62,9 +62,9 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "my_misc_doors:door5 1",
 	recipe = {
-		{"my_door_wood:wood_white", "wool:red", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "wool:red", ""}
+		{"coloredwood:wood_white", "wool:red", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "wool:red", ""}
 	}
 })
 minetest.register_craft({

AntumMT/mp-mydoors

my_misc_doors/locked.lua
@@ -26,18 +26,18 @@ end
 minetest.register_craft({
 	output = "my_misc_doors:door1_locked 1",
 	recipe = {
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", "default:steel_ingot"},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""}
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", "default:steel_ingot"},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""}
 	}
 })
 
 minetest.register_craft({
 	output = "my_misc_doors:door2_locked 1",
 	recipe = {
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", "default:steel_ingot"},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""}
+		{"coloredwood:wood_grey", "coloredwood:wood_grey", ""},
+		{"coloredwood:wood_grey", "coloredwood:wood_grey", "default:steel_ingot"},
+		{"coloredwood:wood_grey", "coloredwood:wood_grey", ""}
 	}
 })
 minetest.register_craft({
@@ -59,9 +59,9 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "my_misc_doors:door5_locked 1",
 	recipe = {
-		{"my_door_wood:wood_white", "wool:red", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", "default:steel_ingot"},
-		{"my_door_wood:wood_white", "wool:red", ""}
+		{"coloredwood:wood_white", "wool:red", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", "default:steel_ingot"},
+		{"coloredwood:wood_white", "wool:red", ""}
 	}
 })
 minetest.register_craft({

my_misc_doors/unlocked.lua
@@ -26,18 +26,18 @@ end
 minetest.register_craft({
 	output = "my_misc_doors:door1 1",
 	recipe = {
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""}
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""}
 	}
 })
 
 minetest.register_craft({
 	output = "my_misc_doors:door2 1",
 	recipe = {
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""},
-		{"my_door_wood:wood_grey", "my_door_wood:wood_grey", ""}
+		{"coloredwood:wood_grey", "coloredwood:wood_grey", ""},
+		{"coloredwood:wood_grey", "coloredwood:wood_grey", ""},
+		{"coloredwood:wood_grey", "coloredwood:wood_grey", ""}
 	}
 })
 minetest.register_craft({
@@ -59,9 +59,9 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "my_misc_doors:door5 1",
 	recipe = {
-		{"my_door_wood:wood_white", "wool:red", ""},
-		{"my_door_wood:wood_white", "my_door_wood:wood_white", ""},
-		{"my_door_wood:wood_white", "wool:red", ""}
+		{"coloredwood:wood_white", "wool:red", ""},
+		{"coloredwood:wood_white", "coloredwood:wood_white", ""},
+		{"coloredwood:wood_white", "wool:red", ""}
 	}
 })
 minetest.register_craft({